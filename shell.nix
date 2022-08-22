24ee1b7e9a4a959e4e71a90b10665840fde5f356
Jack Fletcher <jackowenfletcher@gmail.com>
2022-08-22 21:27:35 +0100

    Update some templates

--- shell.nix
+++ shell.nix
@@ -2,7 +2,7 @@
 
 pkgs.mkShell {
   buildInputs = [
-    pkgs.nodejs-17_x
+    pkgs.nodejs-18_x
     pkgs.yarn
 
     # keep this line if you use bash
--- src/data/recipies.ts
+++ src/data/recipies.ts
@@ -17,7 +17,7 @@ export async function getCooklangRecipieSources() {
     // ssr: false,
   })
 
-  console.log({ files })
+  // console.log({ files })
 
   return files
 }
@@ -26,29 +26,37 @@ export async function getAllRecipies(): Promise<Recipe[]> {
   // Get source file paths and content as an object of keys and values.
   const files = await getCooklangRecipieSources()
 
-  // Initialse recipe classes from the source files.
+  //
   const recipies = Object.entries(files).map(function (file): Recipe {
     const [path, content]: [string, unknown] = file
 
-    // Initialse recipe class from the source file.
+    // Initialise a recipe class from the source file.
     const recipe = new Recipe(String(content))
 
     // TODO: Allow using original values.
     recipe.metadata.path = path
+      .toLowerCase()
+      .replace(/\.cook/g, '') // Trim extension...
+      .replace(/[\s]/g, '-') // Add hyphens...
+      .replace(/[!'()*]/g, '') // Remove other special characters.
+
     recipe.metadata.title = recipe.metadata.path // basename(recipe.metadata.path, '.cook')
+
+    console.log(recipe)
 
     return recipe
   })
 
   const { VITE_RECIPIES_PATH } = import.meta.env
-  console.log({ VITE_RECIPIES_PATH, files, recipies })
+
+  // console.log({ VITE_RECIPIES_PATH, files, recipies })
 
   return recipies
 }
 
 export async function buildRecipePath(recipe: Recipe) {
   return {
-    params: { recipe: recipe?.metadata?.title },
+    params: { recipe: recipe?.metadata?.path },
     props: { recipe: recipe },
   }
 }
