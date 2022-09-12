b0e2ce880b30c8ef65294bab1d3dd3971b58f7de
Jack Fletcher <jackowenfletcher@gmail.com>
2022-09-12 23:22:08 +0100

    Adding some parts to recipe page...

---
+++ src/components/Ingredients.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="recipe-steps">
+    <ul>
+      <li v-for="ingredient in recipe.shoppingList">
+        {{ JSON.stringify(ingredient) }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { Recipe } from '@cooklang/cooklang-ts'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    recipe: Recipe,
+  },
+})
+</script>
---
+++ src/components/Steps.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="recipe-steps">
+    <ul>
+      <li v-for="step in recipe.steps">
+        {{ JSON.stringify(step) }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { Recipe } from '@cooklang/cooklang-ts'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    recipe: Recipe,
+  },
+})
+</script>
