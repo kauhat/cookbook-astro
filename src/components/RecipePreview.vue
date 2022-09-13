082be13b19599f59987cabb2f11f7ecc03693780
Jack Fletcher <jackowenfletcher@gmail.com>
2022-09-14 00:48:28 +0100

    Update styling

--- src/components/RecipePreview.vue
+++ src/components/RecipePreview.vue
@@ -1,11 +1,17 @@
 <template>
-  <div class="recipe-preview prose">
-    <header>
-      <h3 class="title">{{recipe.metadata?.title}}</h3>
-      <p class="servings">Servings: {{recipe.metadata?.servings ?? '??'}}</p>
-    </header>
+  <div class="recipe-preview card bg-base-100 shadow-sm">
+    <div class="card-body prose">
+      <h3 class="title font-display text-2xl font-light">
+        {{ recipe.metadata?.title }}
+      </h3>
+      <p class="servings">Servings: {{ recipe.metadata?.servings ?? '??' }}</p>
 
-    <a :href="recipeUrl">Read more</a>
+      <div class="card-actions justify-end">
+        <a :href="recipeUrl" class="btn btn-outline btn-primary btn-sm"
+          >Read more</a
+        >
+      </div>
+    </div>
   </div>
 </template>
 
