3e088d04932409f738fe8129ce4086f2b197c457
Jack Fletcher <jackowenfletcher@gmail.com>
2022-09-14 00:47:12 +0100

    Add League Spartan typeface

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-    theme: {
-        extend: {},
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        display: ['League Spartan', ...defaultTheme.fontFamily.sans],
+      }
     },
-    plugins: [
-        require('@tailwindcss/typography'),
-        require("daisyui"),
-    ],
+  },
+  plugins: [require('@tailwindcss/typography'), require('daisyui')],
 }
